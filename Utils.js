9e232073130361a28930cc2d754b5d2ee2e2d274
DarkInk <rstonehouse66@gmail.com>
2022-02-15 23:35:56 -0600

    Files

---
+++ Utils.js
@@ -0,0 +1,208 @@
+const crypto = require('crypto')
+const {MessageEmbed} = require('discord.js')
+
+
+/**
+ * @param {MessageEmbed} embed 
+ * @param {Object} body 
+ */
+const issueReopened = async (embed, body) => {
+    const { issue } = body
+    let embedS = embed
+    .setAuthor({ name: issue.user.login, iconURL: issue.user.avatar_url, url: issue.user.html_url })
+    .setTitle(`Issue Reopened: ${issue.title}`)
+    .setURL(issue.html_url)
+    .setDescription(`${_shortner(issue.body)}`)
+    .setColor("PURPLE")
+    .setFooter({ text: `Reopened` })
+    .setTimestamp(issue.created_at)
+
+    return embedS
+}
+
+/**
+ * @param {MessageEmbed} embed 
+ * @param {Object} body 
+ */
+const issueEdited = async (embed, body) => {
+    const { issue, changes } = body
+
+    let embedS = embed
+    .setAuthor({ name: issue.user.login, iconURL: issue.user.avatar_url, url: issue.user.html_url })
+    .setURL(issue.html_url)
+    .setColor("YELLOW")
+    .setFooter({ text: `Edited` })
+    .setTimestamp(issue.updated_at)
+
+    if (changes?.body?.from && changes?.body?.from != issue.body) {
+        embedS.addFields({ name: "From", value: `${_shortner(changes.body.from)}` }, { name: "To", value: `${_shortner(issue.body)}` }).setTitle(`Issue Edited: ${issue.title} (Body)`)
+    }
+
+    if (changes?.title?.from && changes?.title?.from != issue.title) {
+        embedS.addFields({ name: "From", value: `${changes.title.from}` }, { name: "To", value: `${issue.title}` }).setTitle(`Issue Edited: ${issue.title} (Title)`)
+    }
+
+    if (changes?.title?.from == issue.title && changes?.body?.from == issue.body) {
+        embedS.setDescription("No changes made that were noticeable").setTitle(`Issue Edited: ${issue.title} (Unkown)`)
+    }
+
+    return embedS
+}
+
+/**
+ * @param {MessageEmbed} embed 
+ * @param {Object} body 
+ */
+const issueClosed = async (embed, body) => {
+    const { issue } = body
+    let embedS = embed
+    .setAuthor({ name: issue.user.login, iconURL: issue.user.avatar_url, url: issue.user.html_url })
+    .setTitle(`Issue Closed: ${issue.title}`)
+    .setURL(issue.html_url)
+    .setColor("RED")
+    .setFooter({ text: `Closed` })
+    .setTimestamp(issue.closed_at)
+    return embedS
+}
+
+/**
+ * @param {MessageEmbed} embed 
+ * @param {Object} body 
+ */
+const issueCreated = async (embed, body) => {
+    const { issue } = body
+    let embedS = embed
+    .setAuthor({ name: issue.user.login, iconURL: issue.user.avatar_url, url: issue.user.html_url })
+    .setTitle(`Issue Created #${issue.number}: ${issue.title}`)
+    .setURL(issue.html_url)
+    .setDescription(`${_shortner(issue.body)}`)
+    .setColor("ORANGE")
+    .setFooter({ text: `Created` })
+    .setTimestamp(issue.created_at)
+    return embedS
+}
+
+/**
+ * @param {MessageEmbed} embed 
+ * @param {Object} body 
+ */
+const releasePublished = async (embed, body) => {
+    const { release } = body
+    let embedS = embed
+    .setAuthor({ name: release.author.login, iconURL: release.author.avatar_url, url: release.author.html_url })
+    .setTitle(`Release Published: ${release.name}`)
+    .setURL(release.html_url)
+    .setDescription(`${(release.body)}`)
+    .setColor("GREEN")
+    .setFooter({ text: `Published` })
+    .setTimestamp(release.published_at)
+    return embedS
+}
+
+/**
+ * @param {MessageEmbed} embed 
+ * @param {Object} body 
+ */
+const push = async (embed, body) => {
+    const { repository, ref, commits, sender, compare } = body
+    let embedS = embed
+    .setAuthor({ name: sender.login, iconURL: sender.avatar_url, url: sender.html_url })
+    .setTitle(`Push to ${ref.split("/")[2]} | ${commits.length} New ${commits.length > 1 ? "Commits" : "Commit"}`)
+    .setURL(repository.html_url)
+    .setDescription(`${commits.map(commit => `[${commit.id.slice(0, 8)}](${commit.url}) ${commit.message.toString().replaceAll("\n", "")} - ${commit.committer.username}${commits.length > 1 ? "\n" : ""}`).join("")}`)
+    .setColor("BLUE")
+    .setFooter({ text: `Pushed` })
+    .setTimestamp(commits[0].timestamp)
+    return embedS
+}
+
+/**
+ * @param {MessageEmbed} embed 
+ * @param {Object} body 
+ */
+const member_invited = async (embed, body) => {
+    const { invitation, sender, organization } = body
+
+    let embedS = embed
+        .setAuthor({ name: sender.login, iconURL: sender.avatar_url, url: sender.html_url })
+        .setTitle(`Invited to ${organization.login}`)
+        .setURL(organization.url.replace("api.", ""))
+        .setDescription(`${invitation.inviter.login} invited ${invitation.login} to the repository`)
+        .setColor("YELLOW")
+        .setFooter({ text: `Invited` })
+        .setTimestamp(invitation.created_at)
+    return embedS
+}
+
+/**
+ * @param {MessageEmbed} embed 
+ * @param {Object} body 
+ */
+const repository_unarchived = async (embed, body) => {
+    const { repository, sender } = body
+    const embedS = embed
+        
+}
+
+const repository_created = async (embed, body) => {
+    const { repository, sender } = body
+    let embedS = embed
+        .setAuthor({ name: sender.login, iconURL: sender.avatar_url, url: sender.html_url })
+        .setTitle(`Repository Created: ${repository.name}`)
+        .setURL(repository.html_url)
+        .setDescription(`${repository.description ? repository.description : "No description"}`)
+        .setColor("GREEN")
+        .setFooter({ text: `Created` })
+        .setTimestamp(repository.created_at)
+    return embedS
+}
+
+
+const _createComparisonSignature = (body) => {
+    const hmac = crypto.createHmac('sha1', process.env.SECRET)
+    const self_signature = hmac.update(JSON.stringify(body)).digest('hex')
+    return `sha1=${self_signature}`
+}
+
+const _compareSignatures = (signature, comparison_signature) => {
+    const source = Buffer.from(signature)
+    const comparison = Buffer.from(comparison_signature)
+    return crypto.timingSafeEqual(source, comparison)
+}
+
+const verifyGithubPayload = (req, res, next) => {
+    const { headers, body } = req
+
+    const signature = headers['x-hub-signature']
+    const comparison_signature = _createComparisonSignature(body)
+
+    if (!_compareSignatures(signature, comparison_signature)) {
+        return res.status(401).send('Mismatched signatures')
+    }
+    const { action, ...payload } = body
+    req.event_type = headers['x-github-event']
+    req.action = action
+    req.payload = payload
+    next()
+}
+
+
+module.exports = {
+    issueReopened,
+    issueEdited,
+    issueClosed,
+    issueCreated,
+    releasePublished,
+    verifyGithubPayload,
+    push,
+    member_invited,
+    repository_created
+}
+
+function _shortner(text) {
+    if (text && text.length > 500) {
+        return text.substring(0, 500) + "..."
+    } else {
+        return text ? text : "No description"
+    }
+}
---
+++ index.js
@@ -0,0 +1,68 @@
+require("dotenv").config();
+
+const fs = require("node:fs"),
+    path = require("node:path")
+
+const express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+const { push, repository_edited, member_added, create, repository_unarchived, repository_archived, repository_deleted, repository_created, member_invited, releasePublished, issueReopened, issueEdited, issueClosed, issueCreated, verifyGithubPayload } = require('./Utils.js');
+
+
+const { WebhookClient, MessageEmbed } = require("discord.js"),
+    githubclient = new WebhookClient({ url: process.env.webhook })
+
+app.post("/github", verifyGithubPayload, async (req, res) => {
+    try {
+        res.setHeader("X-Powered-By", "CattoLogs")
+
+        const event = req.headers['x-github-event'],
+            eventName = `${event}${req.body.action ? "-" + req.body.action : ""}`;
+
+        fs.writeFileSync(`./Events/github-${eventName}.json`, JSON.stringify(req.body, null, 4));
+
+
+        const eventData = {
+            "issues-reopened": issueReopened,
+            "issues-edited": issueEdited,
+            "issues-closed": issueClosed,
+            "issues-opened": issueCreated,
+            "release-published": releasePublished,
+            "push": push,
+           // "create": create,
+            "organization-member_invited": member_invited,
+           // "organization-member_added": member_added,
+            "repository-created": repository_created,
+            // "repository-deleted": repository_deleted,
+            // "repository-archived": repository_archived,
+            // "repository-unarchived": repository_unarchived,
+        }
+
+        let embed = new MessageEmbed();
+        if (eventData[eventName]) {
+            embed = await eventData[eventName](embed, req.body);
+        } else {
+            //embed.setTitle(`${event}`).setDescription(`Debug:\nNo function for \`${eventName}\``).setColor("RED");
+            return res.status(200).send("No function for " + eventName);
+        }
+
+        githubclient.send({
+            embeds: [embed],
+            username: req.body.repository ? req.body.repository.full_name : "Github",
+            avatarURL: req.body.organization.avatar_url
+        })
+
+        res.send("Successfully posted to Discord");
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({error: true, message: err.message});
+    }
+})
+
+app.listen(process.env.port, () => {
+    console.log('listening on port ' + process.env.port);
+})
