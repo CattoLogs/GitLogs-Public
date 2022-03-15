5699cead1cb7e9f167c1aa773dbb55f595152bbd
FC5570 <fellowcoder5570@gmail.com>
2022-03-15 10:07:02 +0530

    feat: verify github payloads

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import '#lib/setup';
 import { WebhookClient } from 'discord.js';
 import fastify from 'fastify';
 import { setupEvents } from '#lib/setup';
+import { isValidPayload } from '#utils/util';
 
 const client = new WebhookClient({ url: process.env.WEBHOOK_URL! });
 
 const app = fastify({
 	logger: {
 		prettyPrint: {
-			ignore: 'pid,hostname,req,reqid'
+			ignore: 'pid,hostname,req,reqId'
 		},
 		level: process.env.NODE_ENV === 'development' ? 'debug' : 'info'
 	}
@@ -22,14 +23,19 @@ app.post('/github', async (req, res) => {
 	const responseHeaders = { 'x-powered-by': 'CattoLogs', 'user-agent': 'CattoLogs https://github.com/CattoLogs/GitLogs-Public' };
 
 	if (Boolean(process.env.DEBUG)) app.log.info(`Recieved event ${githubEvent}, action ${(req.body as any).action}`);
+
 	try {
 		const event = client.events.get(githubEvent as string);
+
 		if (event) {
-			event.run(req, res, client);
-			return res.status(204);
+			if (process.env.GITHUB_SECRET && isValidPayload(req.headers['x-hub-signature'] as string, req.body)) {
+				event.run(req, res, client);
+			} else {
+				return res.headers(responseHeaders).status(403).send('Invalid signatures.');
+			}
 		}
 
-		return res.headers(responseHeaders).status(404);
+		return res.headers(responseHeaders).status(404).send('Could not find event');
 	} catch (err) {
 		console.log(err);
 		return res
